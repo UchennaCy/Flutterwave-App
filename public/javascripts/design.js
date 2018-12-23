f363d7f631970f0f1ae2011e9d21f513efb60c49
Nnamadim Cyril Uchenna <nnamadimuche@gmail.com>
2018-12-23 08:20:00 +0100

    Remove secret key

--- public/javascripts/design.js
+++ public/javascripts/design.js
@@ -5,7 +5,7 @@
 // 			alert(this.responseText);
 // 		}
 // 	};
-// 	xhttp.open("POST", "https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/12345678901?seckey=FLWSECK-00a2e397d839b8c9015b0890e6eb0ca5-X\
+// 	xhttp.open("POST", "https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/12345678901?seckey=FLWSECK-xxxxxxxxxxxxxxxxxxxx-X\
 // 		--header 'content-type: application/json", true);
 // 	xhttp.setRequestHeader("Content-type", "application/json");
 // 	xhttp.send("Your JSON Data Here");
